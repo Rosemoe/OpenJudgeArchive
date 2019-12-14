8ae3e0f8f62f73732cb2474510e4cf2d7c825dd5
Rose2073 <2073412493@qq.com>
2019-12-14 19:39:10 +0800

    Create 省时间.cpp

---
+++ NKZX_NOI_OJ/P1297/省时间.cpp
@@ -0,0 +1,24 @@
+#include<cstdio>
+#include<unordered_map>
+std::unordered_map<int,int> map;
+int num[1000010];
+int main(){
+    int n,x,c,r = 0;
+    scanf("%d",&n);
+    for(int i = 0;i < n;i++){
+        scanf("%d",&c);
+        num[i] = c;
+        map[c] = 1;
+    }
+    scanf("%d",&x);
+    for(int i = 0;i < n;i++){
+        c = num[i];
+        if(map[c] && x - c > 0 && map[x - c] && c != x - c){
+            map[c] = 0;
+            map[x - c] = 0;
+            r++;
+        }
+    }
+    printf("%d",r);
+    return 0;
+}

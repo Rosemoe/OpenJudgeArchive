9173aeda002eecc5d4d9b9f896041ed306bc56ba
Rose2073 <2073412493@qq.com>
2019-12-14 21:34:21 +0800

    Create P1200.cpp

---
+++ NKZX_NOI_OJ/P1200.cpp
@@ -0,0 +1,35 @@
+#include<cstdio>
+int max;
+long long s;
+bool available[21] = {};
+void test(int values[],int index){
+    for(int i=0;i<21;i++){
+        if(available[i] /*&& (index==0?true:i>values[index-1])*/){
+            values[index] = i;
+            available[i]=0;
+            if(index == max-1){
+                for(int v=0;v<=index;v++){
+                    printf("%d",values[v]);
+                    putchar(' ');
+                }
+                putchar('\n');
+                s++;
+            }
+            else{
+                test(values,index+1);
+            }
+            available[i]=1;
+        }else{
+            continue;
+        }
+    }
+}
+int main(){
+    int n,c=0;
+    scanf("%d%d",&n,&max);
+    int a[21]={};
+    for(int i=1;i<=n;i++) available[i]=1;
+    test(a,0);
+    printf("total=%lld",s);
+    return 0;
+}

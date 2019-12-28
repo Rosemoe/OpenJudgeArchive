1acc1da99e673941f7c12a05ecc103762d9d4270
Rose2073 <2073412493@qq.com>
2019-12-28 21:12:01 +0800

    Create P1324.cpp

---
+++ NKZX_NOI_OJ/P1324.cpp
@@ -0,0 +1,32 @@
+#include<cstdio>
+int n,k;
+struct mo{
+    int pre,nxt;
+}f[1001];
+void c(){
+    for(int i=1;i<=n;i++){
+        f[i].nxt=i+1;
+        f[i].pre=i-1;
+        if(i==1) f[i].pre=n;
+        if(i==n) f[i].nxt=1;
+    }
+}
+void g(){
+    int ct=n;
+    int i=1;
+    while(ct>1){
+        int m=(k-1)%ct+1;
+        for(int j=m;j>1;j--){
+            i=f[i].nxt;
+        }
+        f[f[i].pre].nxt=f[i].nxt;
+        f[f[i].nxt].pre=f[i].pre;
+        printf("%d ",i);
+        i=f[i].nxt;
+        ct--;
+    }
+    printf("%d",i);
+}
+int main(){
+    scanf("%d%d",&n,&k);c();g();return 0;
+}

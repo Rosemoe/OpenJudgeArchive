18c1fa7631483b65efd8c2cb06303095b0d60542
Rosemoe <2073412493@qq.com>
2020-10-01 22:21:59 +0800

    Create P1226.cpp

---
+++ Luogu/P1226.cpp
@@ -0,0 +1,15 @@
+#include<cstdio>
+long long rest(long long v,const int k,int time) {
+    if(time == 0) return 1;
+    if(time == 1) return v%k;
+    long long r = rest(v,k,time>>1);
+    r = (r * r) % k;
+    if(time&1) r = (r * (v%k))%k;
+    return r;
+}
+int main(){
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    printf("%d^%d mod %d=%lld",a,b,c,rest(a,c,b)%c);
+    return 0;
+}
